5fa8503eb100506770dd2492ba94af97225faf15
Joey Mozo <hoeymozo@gmail.com>
2015-03-06 11:17:48 +0800

    modified:   Gemfile 	modified:   Gemfile.lock 	modified:   app/controllers/students_controller.rb 	modified:   app/views/layouts/.DS_Store 	modified:   app/views/students/_form.html.erb 	modified:   app/views/students/index.html.erb

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: [:show, :edit, :update, :destroy]
+  # before_action :set_student, only: [:show, :edit, :update, :destroy]
 
   # GET /students
   # GET /students.json
@@ -32,49 +32,63 @@ class StudentsController < ApplicationController
   def create
     @student = Student.new(student_params)
 
-    respond_to do |format|
-      if @student.save
-        format.html { redirect_to @student, notice: 'Student was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @student }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @student.errors, status: :unprocessable_entity }
-      end
+    if @student.save
+      redirect_to student_path(@student.id)
+    else
+      render :template => "student/new"
     end
+    # respond_to do |format|
+    #   if @student.save
+    #     format.html { redirect_to @student, notice: 'Student was successfully created.' }
+    #     format.json { render action: 'show', status: :created, location: @student }
+    #   else
+    #     format.html { render action: 'new' }
+    #     format.json { render json: @student.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   # PATCH/PUT /students/1
   # PATCH/PUT /students/1.json
   def update
-    respond_to do |format|
-      if @student.update(student_params)
-        format.html { redirect_to @student, notice: 'Student was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @student.errors, status: :unprocessable_entity }
-      end
+    @student = Student.find(params[:id])
+
+    if @student.update(student_params)
+      redirect_to student_path
+    else
+      render :template => "students/edit"
     end
+    # respond_to do |format|
+    #   if @student.update(student_params)
+    #     format.html { redirect_to @student, notice: 'Student was successfully updated.' }
+    #     format.json { head :no_content }
+    #   else
+    #     format.html { render action: 'edit' }
+    #     format.json { render json: @student.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   # DELETE /students/1
   # DELETE /students/1.json
   def destroy
-    @student.destroy
-    respond_to do |format|
-      format.html { redirect_to students_url }
-      format.json { head :no_content }
-    end
+    Student.find(params[:id]).destroy!
+    redirect_to students_path
+    # @student.destroy
+    # respond_to do |format|
+    #   format.html { redirect_to students_url }
+    #   format.json { head :no_content }
+    # end
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_student
-      @student = Student.find(params[:id])
-    end
+  # private
+  #   # Use callbacks to share common setup or constraints between actions.
+  #   def set_student
+  #     @student = Student.find(params[:id])
+  #   end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def student_params
-      params[:student]
-    end
+  def student_params
+    params.require(:student).permit!
+  end
 end
