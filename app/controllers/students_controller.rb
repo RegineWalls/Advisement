e4ec5ee52d9e01ab197f047e9dc8360ba7710175
Joey Mozo <hoeymozo@gmail.com>
2015-03-06 18:29:22 +0800

    modified:   .DS_Store 	modified:   app/.DS_Store 	modified:   app/controllers/students_controller.rb 	modified:   app/models/student.rb 	modified:   app/views/.DS_Store 	modified:   app/views/students/_form.html.erb 	modified:   app/views/students/index.html.erb 	modified:   app/views/students/show.html.erb

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,27 +8,21 @@ class StudentsController < ApplicationController
     render :template => "students/index"
   end
 
-  # GET /students/1
-  # GET /students/1.json
   def show
     @student = Student.find(params[:id])
     render :template => "students/show"
   end
 
-  # GET /students/new
   def new
     @student = Student.new
     render :template => "students/new"
   end
 
-  # GET /students/1/edit
   def edit
     @student = Student.find(params[:id])
     render :template => "students/edit"
   end
 
-  # POST /students
-  # POST /students.json
   def create
     @student = Student.new(student_params)
 
@@ -37,19 +31,9 @@ class StudentsController < ApplicationController
     else
       render :template => "student/new"
     end
-    # respond_to do |format|
-    #   if @student.save
-    #     format.html { redirect_to @student, notice: 'Student was successfully created.' }
-    #     format.json { render action: 'show', status: :created, location: @student }
-    #   else
-    #     format.html { render action: 'new' }
-    #     format.json { render json: @student.errors, status: :unprocessable_entity }
-    #   end
-    # end
+
   end
 
-  # PATCH/PUT /students/1
-  # PATCH/PUT /students/1.json
   def update
     @student = Student.find(params[:id])
 
@@ -58,27 +42,11 @@ class StudentsController < ApplicationController
     else
       render :template => "students/edit"
     end
-    # respond_to do |format|
-    #   if @student.update(student_params)
-    #     format.html { redirect_to @student, notice: 'Student was successfully updated.' }
-    #     format.json { head :no_content }
-    #   else
-    #     format.html { render action: 'edit' }
-    #     format.json { render json: @student.errors, status: :unprocessable_entity }
-    #   end
-    # end
   end
 
-  # DELETE /students/1
-  # DELETE /students/1.json
   def destroy
-    Student.find(params[:id]).destroy!
+    Student.find(params[:id]).destoy!
     redirect_to students_path
-    # @student.destroy
-    # respond_to do |format|
-    #   format.html { redirect_to students_url }
-    #   format.json { head :no_content }
-    # end
   end
 
   # private
--- app/models/student.rb
+++ app/models/student.rb
@@ -4,4 +4,11 @@ class Student < ActiveRecord::Base
 
   has_many :credits
   has_many :tracks, :through => :credits
+
+  validates :name, presence: true, uniqueness: true
+  validates :id_number, presence: true, :numericality => {:greater_than_or_equal_to => 100000, :message => "Invalid ID Number"}
+  validates :course, presence: true
+  validates :email_address, presence: true
+  
+  # accepts_nested_attributes_for :student
 end
