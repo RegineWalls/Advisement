9af49186a1dd86d68011eed3879a0046f29839de
Joey Mozo <hoeymozo@gmail.com>
2015-03-07 18:27:55 +0800

    modified:   app/models/elective.rb 	modified:   app/views/electives/index.html.erb 	modified:   app/views/electives/new.html.erb

--- app/models/elective.rb
+++ app/models/elective.rb
@@ -4,4 +4,11 @@ class Elective < ActiveRecord::Base
 
   has_many :requirements
   has_many :tracks, :through => :requirements
+
+  validates :name, presence: true, uniqueness: true
+  validates :elective_code, presence: true, uniqueness: true
+  validates :prerequisites, presence: true
+  validates :units, presence: true
+  validates :slots, presence: true
+  validates :semester, presence: true
 end
