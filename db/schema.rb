fd6a71c2728193dd377f86e231b6aaef9cb7fd7c
Joey Mozo <hoeymozo@gmail.com>
2015-03-12 13:58:40 +0800

    working login

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150305131011) do
+ActiveRecord::Schema.define(version: 20150312050201) do
 
   create_table "credits", force: true do |t|
     t.integer  "completed"
@@ -81,5 +81,23 @@ ActiveRecord::Schema.define(version: 20150305131011) do
 
   add_index "transcripts", ["elective_id"], name: "index_transcripts_on_elective_id", using: :btree
   add_index "transcripts", ["student_id"], name: "index_transcripts_on_student_id", using: :btree
+
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
