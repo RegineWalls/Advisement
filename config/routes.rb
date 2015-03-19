31694ea8b4d38262fa1cbd8d567ea26e50aeb32b
geoffe02 <you@example.com>
2015-03-19 21:36:59 +0800

    Revert "working students tab functions"
    This reverts commit 21e0891b83228fd7a557b0f1ec585ea0708ea698.
    
    	modified:   app/controllers/electives_controller.rb
    	new file:   app/views/electives/index.html.erb
    	modified:   config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -49,9 +49,6 @@ Rails.application.routes.draw do
   resources :tracks do
     collection {post :import }
   end
- 
-# match 'electives/:id' => 'electives#show', :constraints  => { :id => /[0-z\.]+/ }
-
   # get "/students", :to => "students#index", as: :students
   # get "/student/:id", :to => "students#show", as: :student
   # get "/students/new", :to => "students#new", as: :new_student
